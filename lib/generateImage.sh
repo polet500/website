430185e98db6e81ba4e09b802445d5d424e47446
p-marco <marco.petolicchio@gmail.com>
2019-06-18 12:46:50 +0200

    .

--- lib/generateImage.sh
+++ lib/generateImage.sh
@@ -5,10 +5,10 @@ author="$(cat item.md | grep -o 'author.*' | cut -f2- -d:)"
 
 
 convert ../../../../assets/img/fb-headline-new.png -size 1200x600! \
--background none -fill black  -font Montserrat-Bold -pointsize 26 caption:"$author" \
--pointsize 68 -gravity northwest -size 900x500! -geometry +120+200 -compose over -composite caption:"$title" \
+-background none -fill black  -font Montserrat-Bold -pointsize 22 caption:"$author" \
+-pointsize 60 -gravity northwest -size 1000x500! -geometry +120+200 -compose over -composite caption:"$title" \
 -gravity southwest -geometry +120-120 -compose over -composite \
-fb-banner.jpg
+banner-fb.jpg
 
 
 echo $title
